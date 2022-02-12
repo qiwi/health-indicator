c3f51b08cb73664a14a515732092deb673ee253a
Anton Golub <antongolub@antongolub.com>
2022-02-12 15:35:44 +0300

    refactor: move to http-status-codes v2

--- src/main/ts/endpoint/statuses.ts
+++ src/main/ts/endpoint/statuses.ts
@@ -1,6 +1,6 @@
-export {
-  OK,
-  NOT_FOUND,
-  SERVICE_UNAVAILABLE,
-  INTERNAL_SERVER_ERROR
-} from 'http-status-codes'
+import { StatusCodes } from 'http-status-codes'
+
+export const OK = StatusCodes.OK
+export const NOT_FOUND = StatusCodes.NOT_FOUND
+export const SERVICE_UNAVAILABLE = StatusCodes.SERVICE_UNAVAILABLE
+export const INTERNAL_SERVER_ERROR = StatusCodes.INTERNAL_SERVER_ERROR
